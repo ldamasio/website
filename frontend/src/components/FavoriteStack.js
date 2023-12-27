7797bd40c35baccc370d04c95185ca6d0dd182d9
Leandro Damasio <ldamasio@gmail.com>
2023-12-27 15:43:21 -0300

    Add Kubernetes, Hadoop, Spark among other technologies to the stack

--- frontend/src/components/FavoriteStack.js
+++ frontend/src/components/FavoriteStack.js
@@ -1,21 +1,41 @@
 function FavoriteStack() {
 
   const tools_webdev = [
-  'Django',
-  'FastAPI',
-  'NodeJS',
-  'ReactJS'
+    'Django',
+    'FastAPI',
+    'NodeJS',
+    'ReactJS'
   ];
-  
-  const tools_data = [
-  'PostgreSQL',
-  'Redis',
-  'MongoDB'
+
+  const tools_database = [
+    'PostgreSQL',
+    'MongoDB',
+    'MySQL'
   ];
-  
+
+  const tools_queues = [
+    'Redis',
+    'RabbitMQ',
+    'Apache Kafka'
+  ];
+
+  const tools_datawarehousing = [
+    'Apache Nifi',
+    'Apache Druid',
+    'Apache Superset',
+  ];
+
+  const tools_deepstorage = [
+    'Apache Hadoop',
+    'Apache Spark',
+    'Apache Hive'
+  ];
+
   const tools_devops = [
-  'Docker',
-  'Docker-compose',
+    'Docker',
+    'Docker-compose',
+    'Kubernetes',
+    'Github Actions'
   ];
 
   return (
@@ -28,9 +48,27 @@ function FavoriteStack() {
         </ul>
       </p>
       <p>
+        <h4>Data Storage</h4>
+        <ul>
+          { tools_database.map( (tool_database) => <li>{ tool_database }</li>) }
+        </ul>
+      </p>
+      <p>
+        <h4>Queues</h4>
+        <ul>
+          { tools_queues.map( (tool_queues) => <li>{ tool_queues }</li>) }
+        </ul>
+      </p>
+      <p>
         <h4>Data Warehousing</h4>
         <ul>
-          { tools_data.map( (tool_data) => <li>{ tool_data }</li>) }
+          { tools_datawarehousing.map( (tool_datawarehousing) => <li>{ tool_datawarehousing }</li>) }
+        </ul>
+      </p>
+      <p>
+        <h4>Deep Stoage</h4>
+        <ul>
+          { tools_deepstorage.map( (tools_deepstorage) => <li>{ tools_deepstorage }</li>) }
         </ul>
       </p>
       <p>
